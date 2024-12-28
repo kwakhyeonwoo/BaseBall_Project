ce5d9294211f1ad4753f3e5875eeb82b75b6a130
곽현우 <polla723@naver.com>
2024-12-28 19:27:01 +0900

    Initial Commit

---
+++ Baseball_Project/Baseball_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_ProjectApp.swift
+//  Baseball_Project
+//
+//  Created by 곽현우 on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Baseball_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baseball_ProjectTests/Baseball_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_ProjectTests.swift
+//  Baseball_ProjectTests
+//
+//  Created by 곽현우 on 12/28/24.
+//
+
+import Testing
+@testable import Baseball_Project
+
+struct Baseball_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
