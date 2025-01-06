04473750076fa1efe6789b59999975bd22b8002e
곽현우 <polla723@naver.com>
2025-01-06 14:09:15 +0900

    Update .gitignore

---
+++ Baseball/Model/TeamScheduleModel.swift
@@ -0,0 +1,8 @@
+//
+//  TeamScheduleModel.swift
+//  Baseball
+//
+//  Created by 곽현우 on 1/5/25.
+//
+
+import Foundation
---
+++ Baseball/ViewModel/GameScheduleViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  TeamScheduleViewModel.swift
+//  Baseball
+//
+//  Created by 곽현우 on 1/5/25.
+//
+
+import SwiftUI
+
+class GameScheduleViewModel: ObservableObject {
+    @Published var gameSchedules: [TeamScheduleModel] = []
+    
+    func fetchGameSchedules(for team: String) {
+        guard let url = URL(string: "https://api.naver.com/baseball/games/\(team)") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("gaQ9abn61x6i6MsxWf8m", forHTTPHeaderField: "X-Naver-Client-Id")  // API 키 설정
+        request.setValue("j7Yc0FLwRh", forHTTPHeaderField: "X-Naver-Client-Secret")  // 비밀키 설정
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let gameResponse = try decoder.decode(GameResponse.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.gameSchedules = gameResponse.games
+                }
+            } catch {
+                print("Error decoding data: \(error)")
+            }
+        }.resume()
+    }
+}
+
+
